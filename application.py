15b49bf10fb52f7b460e75a84820c27bf7be47e9
Steven <stevenddelaney@gmail.com>
2019-10-13 15:32:26 -0500

    Added socketIO functionality - functional msging

--- application.py
+++ application.py
@@ -1,13 +1,56 @@
 import os
 
-from flask import Flask, render_template
-# from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
+from helpers import apology, login_required
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-# app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-# socketio = SocketIO(app)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
-@app.route("/")
+txt = {"engineering": ['test']}
+
+@app.route("/", methods=["GET", "POST"])
+@login_required
 def index():
-    return render_template('index.html')
+    if request.method == "GET":
+        return render_template('index.html', items=txt["engineering"])
+    else:
+        #add li to page
+        return render_template('index.html', items=txt["engineering"])
+
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    session.clear()
+    if request.method == "GET":
+        return render_template('register.html')
+
+    else:
+        session["name"] = request.form.get("register")
+        return redirect("/")
+
+
+@socketio.on("submit text")
+def vote(data):
+    txt[data["channel"]].append(data["info"])
+    emit("channel text", data, broadcast=True)
--- static/main.js
+++ static/main.js
@@ -7,6 +7,32 @@ window.onload=function () {
 }
 
 
+document.addEventListener('DOMContentLoaded', () => {
+   // Connect to websocket
+   var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+   // When connected, configure buttons
+   socket.on('connect', () => {
+
+       // Each button should emit a "submit vote" event
+       document.querySelectorAll('form').forEach(form => {
+           form.onsubmit = () => {
+               console.log("hi")
+               const channel = form.dataset.channel;
+               const info = form.querySelector('#sendtext').value;
+               socket.emit('submit text', {'channel': channel, 'info': info});
+           };
+       });
+   });
+
+   // When a new vote is announced, add to the unordered list
+   socket.on('channel text', data => {
+       document.querySelector('.list-group-mine').innerHTML += '<li class="list-group-item">' + data["info"] + '</li>'
+       var objDiv = document.getElementById("bottom-up");
+       objDiv.scrollTop = objDiv.scrollHeight;
+   });
+
+});
 
 
 
